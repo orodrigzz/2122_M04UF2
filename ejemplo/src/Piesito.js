68c45dd1b662e4b8d1b28f4cfdea03a03e8c1b05
enti <enti@enti.enti>
2022-02-25 14:27:07 +0100

    Clase 25-2

---
+++ ejemplo/src/Piesito.js
@@ -0,0 +1,13 @@
+import './Piesito.css';
+
+function Piesito (){
+	return(
+		<footer>
+		<p> Copy (C) 2022 La mamá de Piesito </p>
+
+		</footer>
+
+	);
+}
+
+export default Piesito;
---
+++ ejemplo/src/Titulito.js
@@ -0,0 +1,10 @@
+function Titulito(){
+	return (
+		<div id="titulo">
+		<h1> ola k ase </h1>
+		<p> Best title en the guorl </p>
+		</div>
+	);
+}
+
+export default Titulito;
